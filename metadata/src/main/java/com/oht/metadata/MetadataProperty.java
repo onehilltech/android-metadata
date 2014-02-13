4486d13122dbd3b59786feb657977afd1a287a06
hilljh82 <hilljh82@gmail.com>
2014-02-12 22:58:11 -0400

    Initial commit

---
+++ metadata/src/main/java/com/oht/metadata/MetadataProperty.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2013, James H. Hill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.oht.metadata;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @interface Metadata
+ * 
+ * Metadata annotation for class attributes.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface MetadataProperty
+{
+  /// Name of the metadata.
+  String name () default "";
+  
+  /// The metadata value comes from a resource.
+  boolean fromResource () default false;
+  
+  /// Get the hint for the resource type.
+  ResourceType resourceType () default ResourceType.Auto;
+}
